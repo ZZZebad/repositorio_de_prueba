ffe6f74eb7bc216acae962ec737b2e833d1d1836
ZZZebad <sebadelga2000@gmail.com>
2026-02-07 21:21:10 -0300

    rama agregada

---
+++ analisis.py
@@ -0,0 +1,8 @@
+import math
+
+def calcular_area_circulo(radio):
+    return math.pi * radio**2
+
+print(f"El área es: {calcular_area_circulo(5)}")
+
+# Objetivo: Alcanzar nivel B2 de inglés este año para mejorar mis reportes
